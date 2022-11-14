7086bb32daf426f9e5f95a048f36aed6efb42c6d
gildsoncarvalho <gildson.carvalho@doutornature.com>
2022-11-14 16:11:45 -0300

    create file for insert code

---
+++ assets/js/footer.js
@@ -0,0 +1,13 @@
+var footer = document.getElementById('footer')
+footer.innerHTML = '<div class="container">\
+                      <p class="text-center text-white-50 mb-0">\
+                        <small>&copy; <span id="ano"></span> Doutor Nature - Todos os Direitos Reservados <br>\
+                          <a href="https://doutornature.com/politica-de-privacidade/" target="blank_" class="text-white">Políticade Privacidade</a>\
+                          <span class="text-white">&nbsp;|&nbsp;</span>\
+                          <a href="https://doutornature.com/termos-de-uso/" target="blank_" class="text-white">Termos deUso</a>\
+                        </small>\
+                      </p>\
+                      <p class="text-center mt-0">\
+                        <small>Produto dispensado da obrigatoriedade de registro conforme RDC 52/2010.</small>\
+                      </p>\
+                    </div>';
